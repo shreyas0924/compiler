79ab75621718195398ede0255858f5cb367d54a7
shreyas0924 <shreyasp0924@gmail.com>
2023-09-19 13:37:04 +0530

    routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 // import React from 'react'
 
-import CodeEditor from './components/CodeEditor'
+import { Link } from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
       Main page
-      <CodeEditor />
+      <button className='border-2 p-2 rounded-2xl'>
+        <Link to='/editor'>Go to code editor</Link>
+      </button>
     </div>
   )
 }
